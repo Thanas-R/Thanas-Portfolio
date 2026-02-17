1ce348e716e1464639cfdd14f7c1bb846519865e
Thanas R <thanas5.rd@gmail.com>
2026-02-17 17:13:35 +0000

    Reverted to commit 527e2ad42f20da75d726716a1efb6df584963d69

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,8 +14,8 @@ const Index = () => {
       <div className="relative z-10">
         <Navbar />
         <HeroSection />
-        <TechCarousel />
         <AboutSection />
+        <TechCarousel />
         <ProjectsSection />
         <ResumeSection />
         <ContactSection />
