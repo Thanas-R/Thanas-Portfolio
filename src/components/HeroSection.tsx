ecc9e4806978598229cb4815a1b03decefc73901
Thanas R <thanas5.rd@gmail.com>
2026-02-18 12:59:36 +0000

    Visual edit in
    Edited UI in

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -167,7 +167,7 @@ const HeroSection = () => {
                 e.currentTarget.style.setProperty('--xRotation', `${xRot}deg`);
                 e.currentTarget.style.setProperty('--yRotation', `${yRot}deg`);
               }}
-              className="relative w-48 h-48 md:w-60 md:h-60 glow-card rounded-2xl transition-transform ease-out hover:[transform:rotateX(var(--xRotation))_rotateY(var(--yRotation))_scale(1.05)] overflow-hidden border-white border-4 border-solid">
+              className="relative w-48 h-48 md:w-60 md:h-60 glow-card rounded-2xl transition-transform ease-out hover:[transform:rotateX(var(--xRotation))_rotateY(var(--yRotation))_scale(1.05)] overflow-hidden border-4 border-solid border-primary">
 
               <img
                 src={avatar}
