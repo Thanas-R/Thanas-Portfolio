3a8832ec2a3f7f799c81334c511d2f81cd498ed3
Thanas R <thanas5.rd@gmail.com>
2026-02-20 05:13:46 +0000

    Revamped components cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -57,12 +57,13 @@ const AnimatedRoutes = () => {
         exit={{ opacity: 0, y: -12 }}
         transition={{ duration: 0.3, ease: 'easeInOut' }}
       >
-        <Routes location={location}>
-          <Route path="/" element={<Index />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/resume" element={<ResumePage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+          <Routes location={location}>
+            <Route path="/" element={<Index />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/projects/:slug" element={<ProjectDetailPage />} />
+            <Route path="/resume" element={<ResumePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
       </motion.div>
     </AnimatePresence>
   );
