136ed785e4ab88d190a2669d772f41a1da607065
Thanas R <thanas5.rd@gmail.com>
2026-02-23 21:46:37 +0530

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -146,7 +146,7 @@ const ContactSection = () => {
     text-foreground text-lg font-medium dark:font-semibold 
     placeholder:text-muted-foreground/70 placeholder:font-medium 
     focus:outline-none focus:ring-1 focus:ring-ring 
-    resize-y mb-6 min-h-[210px] overflow-auto scrollbar-hide"
+    resize-y mb-6 min-h-[200px] overflow-auto scrollbar-hide"
     required
   />
 
