f2975bf1e19c6ed7a3486bad514b855a7c6bfe13
Thanas R <thanas5.rd@gmail.com>
2026-03-08 15:42:32 +0000

    Add command palette UI
    Co-authored-by: Thanas-R <220031267+Thanas-R@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -104,6 +104,7 @@ const App = () => (
       <GlowCardPointerTracker />
       <BrowserRouter>
         <HashScrollHandler />
+        <CommandPalette />
         <AnimatedRoutes />
       </BrowserRouter>
     </TooltipProvider>
