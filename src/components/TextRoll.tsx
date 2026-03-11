366058ec349afb1b01971547418861a5c6418b7c
Thanas R <thanas5.rd@gmail.com>
2026-03-11 10:19:46 +0530

    Update TextRoll.tsx

--- src/components/TextRoll.tsx
+++ src/components/TextRoll.tsx
@@ -9,7 +9,7 @@ const TextRoll = ({ children, className, center = false }: { children: string; c
       initial="initial"
       whileHover="hovered"
       className={cn('relative block overflow-hidden', className)}
-      style={{ lineHeight: 1 }}
+      style={{ lineHeight: 1.05 }}
     >
       <div>
         {children.split('').map((l, i) => {
