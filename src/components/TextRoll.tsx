83bfb3915e21ed1e50e74b5e644612be9152e25e
Thanas R <thanas5.rd@gmail.com>
2026-03-11 10:09:34 +0530

    Update TextRoll.tsx

--- src/components/TextRoll.tsx
+++ src/components/TextRoll.tsx
@@ -9,17 +9,18 @@ const TextRoll = ({ children, className, center = false }: { children: string; c
       initial="initial"
       whileHover="hovered"
       className={cn('relative block overflow-hidden', className)}
-      style={{ lineHeight: 0.85 }}
+      style={{ lineHeight: 0.85, paddingTop: '2px', paddingBottom: '2px' }}
     >
       <div>
         {children.split('').map((l, i) => {
           const delay = center
             ? STAGGER * Math.abs(i - (children.length - 1) / 2)
             : STAGGER * i;
+
           return (
             <motion.span
               key={i}
-              variants={{ initial: { y: 0 }, hovered: { y: '-90%' } }}
+              variants={{ initial: { y: 0 }, hovered: { y: '-95%' } }}
               transition={{ ease: 'easeInOut', delay }}
               className="inline-block"
             >
@@ -29,15 +30,16 @@ const TextRoll = ({ children, className, center = false }: { children: string; c
         })}
       </div>
 
-      <div className="absolute inset-0 top-[2px]">
+      <div className="absolute inset-0">
         {children.split('').map((l, i) => {
           const delay = center
             ? STAGGER * Math.abs(i - (children.length - 1) / 2)
             : STAGGER * i;
+
           return (
             <motion.span
               key={i}
-              variants={{ initial: { y: '90%' }, hovered: { y: 0 } }}
+              variants={{ initial: { y: '105%' }, hovered: { y: 0 } }}
               transition={{ ease: 'easeInOut', delay }}
               className="inline-block"
             >
