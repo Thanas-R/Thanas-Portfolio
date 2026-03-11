e6e13d7d0cb4ce6d54491bf104f8de19a8a5eed3
Thanas R <thanas5.rd@gmail.com>
2026-03-11 16:55:30 +0000

    Reverted to commit 5e7d526e242b63db283bc4655c0bc46b8a2a6d1d

--- src/components/GradientCard.tsx
+++ src/components/GradientCard.tsx
@@ -14,7 +14,7 @@ const GradientCard = ({ children, className }: GradientCardProps) => {
         spread={40}
         disabled={false}
         proximity={64}
-        borderWidth={1.5}
+        borderWidth={1}
         blur={0}
       />
       <div className="relative z-10 h-full">{children}</div>
