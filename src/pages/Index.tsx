308aa33841666529cf09667771aa140ecc2e892a
Thanas R <thanas5.rd@gmail.com>
2026-03-15 22:48:54 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,10 +5,16 @@ import AboutSection from '@/components/AboutSection';
 import TechCarousel from '@/components/TechCarousel';
 import ProjectsSection from '@/components/ProjectsSection';
 import ContactSection from '@/components/ContactSection';
+import SEOHead from '@/components/SEOHead';
 
 const Index = () => {
   return (
     <>
+      <SEOHead
+        title="Thanas R"
+        description="Developer & creative problem-solver. Building thoughtful digital experiences with code."
+        path="/"
+      />
       <TopographicBackground />
       <div className="relative z-10 overflow-x-hidden">
         <Navbar />
