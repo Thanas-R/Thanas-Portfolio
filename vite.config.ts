7fbbe9e1c2bc0ad5e3b4ff1590587eca31e4d904
Thanas R <thanas5.rd@gmail.com>
2026-05-15 19:18:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === "development" && componentTagger(),
     mode !== "development" &&
+    !process.env.VERCEL &&
 
 prerender({
       routes: [
